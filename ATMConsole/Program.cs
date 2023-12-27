5a340687aef1003f2882eedbd7285686e4d075ab
Jayden <59065556+Jdare2001@users.noreply.github.com>
2023-12-27 21:46:31 +0000

    Set up main menu

--- ATMConsole/Program.cs
+++ ATMConsole/Program.cs
@@ -1,6 +1,39 @@
-﻿public class ATMConsoleApp{
+﻿
+
+
+
+public class ATMConsoleApp{
+    
+    
 
     public static void Main(string[] args){
-        Console.WriteLine("Hello World");
+        Menu();
     }
+    private static void Menu(){
+    bool optionchosen = false;
+    string selection;
+    while(!optionchosen){
+        Console.WriteLine("Welcome to the Dare Bank ATM");
+        Console.WriteLine("please select an option");
+        Console.WriteLine("1 - Input pin");
+        Console.WriteLine("2 - Create an account");
+        Console.WriteLine("3 - Quit");
+        selection = Console.ReadLine().ToString();
+        switch(selection){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                Environment.Exit(0);
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
+    }
+
 }
